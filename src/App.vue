f92579f82f3195e1d781bf4d140cdbd6e658a6af
FuyaZP3 <fuyazp3@gmail.com>
2025-09-16 01:01:10 +0300

    update colorMap of FButton

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,17 @@
 </script>
 
 <template>
-  <div class="mx-auto p-5  min-h-screen">
-    <FButton label="Save User" color="warning"/>
-    <button
-      type="button"
-      class="bg-[#0D9488] px-2 py-1 rounded-4xl text-white hover:bg-green-600 active:bg-green-700"
-    >Login</button>
+  <div class="mx-auto p-5  min-h-screen bg-gray-300">
+    <div class="flex justify-evenly flex-col *:m-2">
+
+      <FButton label="Save User" color="success"/>
+      <FButton label="Save User" color="warning"/>
+      <FButton label="Save User" color="danger"/>
+      <FButton label="Save User" color="success" disabled/>
+      <FButton label="Save User" color="warning" disabled/>
+      <FButton label="Save User" color="danger" disabled/>
+      
+    </div>
   </div>
 </template>
 
--- src/components/FButton.vue
+++ src/components/FButton.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
+type Colors = 'success' | 'warning' | 'danger'
+
 interface Props {
   label?: string
   variant?: 'filled' | 'outlined'
-  color?: 'success' | 'warning' | 'danger'
+  color?: Colors
   disabled?: boolean
   type?: 'button' | 'submit' | 'reset'
 }
@@ -15,14 +17,24 @@ const classes = computed(() => {
   const base = 'inline-flex justify-center align-middle items-center px-4 py-2'
 
   const colors: Record<NonNullable<Props['color']>, string> = {
-    success: 'bg-green-500 hover:bg-green-600 active:bg-green-700 text-white disabled:bg-gray-500',
-    warning: 'bg-yellow-500 hover:bg-yellow-600 active:bg-yellow-700 text-black',
-    danger: 'bg-red-500 hover:bg-red-600 active:bg-red-700 text-white'
+    success: `
+      bg-green-500 text-white
+      hover:bg-green-600 active:bg-green-700
+      disabled:opacity-50 disabled:hover:bg-green-500 disabled:active:bg-green-500
+    `,
+    warning: `
+      bg-yellow-500 text-black
+      hover:bg-yellow-600 active:bg-yellow-700
+      disabled:opacity-50 disabled:hover:bg-yellow-500 disabled:active:bg-yellow-500
+    `,
+    danger: `
+      bg-red-500 text-white
+      hover:bg-red-600 active:bg-red-700
+      disabled:opacity-50 disabled:hover:bg-red-500 disabled:active:bg-red-500
+    `,
   }
-  
-  const disabled = props.disabled ? 'opacity-50 cursor-not-allowed' : ''
 
-  return [base, colors[props.color ?? 'primary'], disabled]
+  return [base, colors[props.color ?? 'success']]
 })
 </script>
 
